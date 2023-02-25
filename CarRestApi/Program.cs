a1dcf4d05310f881160f38d893dd652cffd4bc14
Lasse Kristensen <lass580n@edu.zealand.dk>
2023-02-25 20:26:06 +0100

    add fix

--- CarRestApi/Program.cs
+++ CarRestApi/Program.cs
@@ -13,11 +13,14 @@ builder.Services.AddSingleton<CarRepository>(new CarRepository());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
